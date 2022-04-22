a6e91a23a0dfdca0f8d55b58655b306e15df0f02
BlazOsredkar <blaz.osredkar@scv.si>
2022-04-22 17:15:20 +0200

    title updatan

--- frontend/src/components/title.tsx
+++ frontend/src/components/title.tsx
@@ -5,7 +5,7 @@ const Title = () => {
       <section className="py-5 text-center container">
           <div className="row py-lg-5">
               <div className="col-lg-6 col-md-8 mx-auto">
-                  <h1 className="fw-light">Blog, ko ga ne bom nikol objavu</h1>
+                  <h1 className="fw-light">Blog </h1>
                   <p className="lead text-muted">Something short and leading about the collection below—its contents, the
                       creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it
                       entirely.</p>
